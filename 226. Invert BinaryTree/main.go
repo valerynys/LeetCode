545c86b00457190d31e8acdaff9078b5f4f8147b
valerynys <vvvo2002@mail.ru>
2023-02-26 00:20:13 +0300

    226 021

---
+++ 226. Invert BinaryTree/main.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root != nil {
+		root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	}
+	return root
+}
+
+func main() {
+
+}
